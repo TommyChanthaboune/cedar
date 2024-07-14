dfe8303cda13656a8353e0207814c8f39c8f9b93
Tommy Chanthaboune <tchanthaboune@gmail.com>
2024-07-14 00:24:13 -0500

    small update to step padding

--- src/components/Step/Step.tsx
+++ src/components/Step/Step.tsx
@@ -9,7 +9,7 @@ type TStep = (props: TStepProps) => React.ReactElement;
 
 const Step: TStep = ({ step, label, active, edit = null }) => {
   return (
-    <div className="flex items-center bg-white px-5 py-6">
+    <div className="flex items-center bg-white px-8 py-6">
       <div className="mr-4 size-6">
         <div
           className={`${active ? 'bg-ctaBlue text-white' : 'bg-cedarLightGray text-cedarDarkerGray'} flex items-center justify-center rounded-full font-bold text-white`}
